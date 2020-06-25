b86e2fd8dc9fff6dd2d4f2e950349298c439fa24
Isaias Del Mundo <idelmundo68@gmail.com>
2020-06-24 19:15:47 -0700

    added html and js

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1 Display current Date
+
+// code drill 04 local Storage
+
+// 2 Present timeblock in html via JSON
+
+// 3 figure out how to shade time blocks past present future
+
+// 4 click time block can enter text event 
+
+// 5 save the event 
+
+// 6 when page refresh the s
+
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+})
